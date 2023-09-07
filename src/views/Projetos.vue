dfd2a19fa8d3b7b594fdde19187029aeef94eeac
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2023-09-07 03:41:18 -0300

    feat: create a way to register projects

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,12 +1,57 @@
 <template>
-    <h1>Projetos</h1>
+  <section class="projetos p-5">
+    <h1 class="title">Projetos</h1>
+    <form @submit.prevent="salvar">
+        <div class="field">
+            <label class="label" for="nomeDoProjeto">
+                Nome do projeto
+            </label>
+            <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjeto"/>
+        </div>
+        <div class="field">
+            <button class="button" type="submit">
+                Salvar
+            </button>
+        </div>
+    </form>
+    <table class="table is-fullwidth mt-5">
+        <thead>
+            <tr>
+                <th> ID</th>
+                <th> Nome do Projeto</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="projeto in projetos" :key="projeto.id">
+                <td> {{ projeto.id}} </td>
+                <td> {{ projeto.nome }} </td>
+            </tr>
+        </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import type IProjeto from "@/interfaces/IProjeto";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'Projetos'
-})
+  name: "Projetos",
+  data(){
+    return{
+        nomeDoProjeto: '',
+        projetos: [] as IProjeto[]
+    }
+  },
+  methods:{
+    salvar(){
+        const projeto: IProjeto = {
+            nome: this.nomeDoProjeto,
+            id: new Date().toISOString(),
+        }
+        this.projetos.push(projeto)
+        this.nomeDoProjeto = ''
+    }
+  }
+});
 </script>
